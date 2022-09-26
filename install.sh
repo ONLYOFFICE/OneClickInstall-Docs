fd78db256ce20c8b0422680bfc5696d1f1bf22ca
evgeniy-antonyuk <antonyuk.evgenyiy@onlyoffice.com>
2022-09-26 09:27:01 +0300

    Revert "Fix docker installation for ARM"
    This reverts commit 9f1986a7d90a9e34ed0f1ba82c32679f814f4217.

--- install.sh
+++ install.sh
@@ -608,17 +608,13 @@ get_available_version () {
 		TAGS_RESP=$(echo $TAGS_RESP | jq -r '.results[].name')
 	fi
 
-	if [[ "${MACH}" = "aarch64" || "${MACH}" = "arm64" ]]; then
-		ARCHITECTURE_REGEX='^.+(arm64)+$'
-	else
-		ARCHITECTURE_REGEX='^[^arm64]+$'
-	fi
-	
+	VERSION_REGEX_1="[0-9]+\.[0-9]+\.[0-9]+"
+	VERSION_REGEX_2="[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+"
 	TAG_LIST=""
 
 	for item in $TAGS_RESP
 	do
-		if [[ $item =~ $ARCHITECTURE_REGEX ]]; then 
+		if [[ $item =~ $VERSION_REGEX_1 ]] || [[ $item =~ $VERSION_REGEX_2 ]]; then
 			TAG_LIST="$item,$TAG_LIST"
 		fi
 	done
