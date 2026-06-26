efca68ae1b427bc9988fd95a9ceadc5036ea01b5
Vladimir Ischenko <vladimir.ischenko@onlyoffice.com>
2026-06-26 11:21:11 +0000

    Fix nginx default config conflict on DEB update (#135)
    Reviewed-on: https://git.onlyoffice.com/ONLYOFFICE/OneClickInstall-Docs/pulls/135
    Co-authored-by: Vladimir Ischenko <vladimir.ischenko@onlyoffice.com>
    Co-committed-by: Vladimir Ischenko <vladimir.ischenko@onlyoffice.com>

--- install-Debian/install-app.sh
+++ install-Debian/install-app.sh
@@ -110,8 +110,11 @@ if [ "$DOCUMENT_SERVER_INSTALLED" = "false" ]; then
     apt-get install -yq ${ds_pkg_name}
 
     [ -e /etc/nginx/sites-enabled/default ] && \
-        mv -f /etc/nginx/sites-enabled/default /etc/nginx/sites-available/default.disabled && \
-        echo "Note: nginx default site disabled to avoid conflict with ${ds_pkg_name}."
+        mv -f /etc/nginx/sites-enabled/default /etc/nginx/sites-available/default.disabled
+    [ -e /etc/nginx/conf.d/default.conf ] && \
+        mv -f /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.conf.disabled
+    echo "Note: nginx default sites disabled to avoid conflict with ${ds_pkg_name}."
+    service nginx reload
 fi
 
 echo ""
